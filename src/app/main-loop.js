511be6023471d086934fe5c4b69160f48a4d1014
mptYa <19mtp84@gmail.com>
2017-10-11 00:06:39 +0300

    some changes

--- src/app/main-loop.js
+++ src/app/main-loop.js
@@ -1,26 +1,26 @@
 import elements from './dom-elements';
 
 export default function(numberOfPlayers) {
-  const ctx = elements.canvas.getContext('2d');
-  const gradient = ctx.createLinearGradient(0, 0, 0, 512);
+  const context = elements.canvas.getContext('2d');
+  const gradient = context.createLinearGradient(0, 0, 0, 512);
 
-  ctx.canvas.style.display = 'block';
-  ctx.canvas.style.margin = '20px auto';
-  ctx.canvas.style.border = '2px solid #7c7c7c';
+  context.canvas.style.display = 'block';
+  context.canvas.style.margin = '20px auto';
+  context.canvas.style.border = '2px solid #7c7c7c';
 
-  ctx.strokeStyle = '#298f48';
-  ctx.lineWidth = 5;
+  context.strokeStyle = '#298f48';
+  context.lineWidth = 5;
 
   gradient.addColorStop(0, '#dbb941');
   gradient.addColorStop(0.4, '#34a5be');
   gradient.addColorStop(0.7, '#cf41db');
-  ctx.fillStyle = gradient;
+  context.fillStyle = gradient;
 
-  ctx.beginPath();
-  ctx.moveTo(256, 128);
-  ctx.lineTo(384, 384);
-  ctx.lineTo(128, 384);
-  ctx.closePath();
-  ctx.fill();
-  ctx.stroke()
+  context.beginPath();
+  context.moveTo(256, 128);
+  context.lineTo(384, 384);
+  context.lineTo(128, 384);
+  context.closePath();
+  context.fill();
+  context.stroke()
 };
